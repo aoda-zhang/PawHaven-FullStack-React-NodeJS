c121da1374beee833790510cd32e8e0df180d046
aoda <aoda2826@qq.com>
2026-04-23 20:06:37 +0800

    fix(.github/actions/vercel-deploy): correct service path variable and output

--- apps/frontend/portal/src/features/Home/apis/requests.ts
+++ apps/frontend/portal/src/features/Home/apis/requests.ts
@@ -3,9 +3,9 @@ import type { RescueItem } from '@pawhaven/shared/types';
 import { apiClient } from '@/utils/apiClient';
 
 export const getLatestRescuesByNumber = (): Promise<RescueItem[]> => {
-  return apiClient.get('/core-service/rescues');
+  return apiClient.get('/core/rescues');
 };
 
 export const getLatestStories = (): Promise<RescueItem[]> => {
-  return apiClient.get('/core-service/rescues');
+  return apiClient.get('/core/rescues');
 };
